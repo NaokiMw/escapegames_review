090dc0d8c78f64513858e9456196b5acf605389e
maekawanaoki <personalpeach10@icloud.com>
2022-05-17 15:17:40 +0900

    add: reviewsコントローラーにcreateメソッドとshowメソッドを追加 fix: indexメソッドを全体が見れるように変更 内容: ログインユーザーのレビューのみはshow,全体はindexと使い分ける

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,26 @@
 class ReviewsController < ApplicationController
   def index
-    @user = current_user.id
     @reviews = Review.all
   end
 
   def new
-    
+    @user = current_user.id
     @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      flash[:notice] = "レビューを新規作成しました"
+      redirect_to :reviews
+    else
+      rendaer"new"
   end
 
   def show
+    @user = current_user.id
+    @review = Review.where(user_id: @user)
   end
 
   def edit
@@ -23,4 +31,8 @@ class ReviewsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def review_params
+    params.require(:review).permit(:title, :review_1, :review_2, :review_3, :review_4, :review_5, :place, :play_day, :start_time, :result, :image, :user_id)
 end
