db451fe7f15ac7179808bae5c16617568692b6a9
maekawanaoki <personalpeach10@icloud.com>
2022-05-23 15:55:30 +0900

    add: プロフィール機能の実装 内容:profileのカラムをusesに追加、userコントローラーとshowページ共に追加。     editにてプロフィールを記載できるように追加。 update: editページの日本語化と、usernameの修正ができるように実装 内容:英語で書かれていた説明を日本語に修正、usernameも編集できるように追記。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :username, presence: true 
+  validates :profile, length: { maximum: 200 } 
   has_many :reviews
   has_many :favorites
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root 'reviews#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  
   resources :reviews do
     resource :favorites, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
