eed9b1a6b949ca9b23239ecb52d9a5e33fd44778
maekawanaoki <personalpeach10@icloud.com>
2022-06-10 18:25:54 +0900

    add: diaryのrequestメソッドを追加

---
+++ spec/factories/diary.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+    factory :diary do
+      user_id {"1"}
+      title {"rspecからの脱出"}
+      description {"楽しかった"}
+      play_day {"2022-11-27"}
+      start_time {"18:15"}
+      result {"true"}
+      status {"draft"}
+    end
+  end
