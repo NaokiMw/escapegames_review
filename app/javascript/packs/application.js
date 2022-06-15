42ad5f9a20d31e4ac41b36aeaf9692b20d70d100
maekawanaoki <personalpeach10@icloud.com>
2022-06-16 00:29:04 +0900

    test: bootstrapを読み込まなくなったので、テスト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import '@fortawesome/fontawesome-free/js/all';
 import 'bootstrap';
+import '../stylesheets/application';
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
