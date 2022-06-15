b485fc9fe0c66a0262e37df36a4b4dce2ecdb990
maekawanaoki <personalpeach10@icloud.com>
2022-06-15 18:24:28 +0900

    fix: created_timeをcreated_atに修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews.order(:created_at).limit(5)
-    @diary = @user.diaries.order(:updated_time).limit(5)
+    @diary = @user.diaries.order(:updated_at).limit(5)
     @favorite_reviews = @user.favorite_reviews
     followings = current_user.followings
     @follow_reviews = Review.where(user: followings).order(:created_at).limit(10)
