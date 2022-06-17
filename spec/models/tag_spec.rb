e19617cfa7f093c687e6517fb8d5ed4a5da71a2d
maekawanaoki <personalpeach10@icloud.com>
2022-06-18 00:32:07 +0900

    add: rspecの追加

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Tag, type: :model d
+  before do
+    @tag = build(:tag)
+  end
+
+  describe 'バリデーション' do
+    it '全ての値が設定されている場合、タグが作成されること' do
+      expect(@tag.valid?).to eq(true)
+    end
+  end
 end
