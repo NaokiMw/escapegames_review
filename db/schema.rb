b85ba74e60e30c8fea70919b1b86937a5471a4b8
maekawanaoki <personalpeach10@icloud.com>
2022-06-18 14:04:35 +0900

    add: reviewにカラムを４つ追加。 update: レビューの新規作成ページのフォームを追加、一部ラジオボタンからセレクトボックスに変更
    内容:
    料金、制限時間、プレイ人数、一緒に遊んだ友人・知り合いの項目を追加。
    満足度などの質問をラジオボタンにしていると、選択肢を増やすのが難しいため、
    拡張性を考慮して選択ボックスに変更した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_120038) do
+ActiveRecord::Schema.define(version: 2022_06_17_155511) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -81,6 +81,10 @@ ActiveRecord::Schema.define(version: 2022_06_17_120038) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price"
+    t.integer "timelimit"
+    t.integer "players"
+    t.integer "friends"
   end
 
   create_table "tag_maps", force: :cascade do |t|
