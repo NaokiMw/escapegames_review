5ebd7299989ff0d32b5831a41d442eff7046db3b
maekawanaoki <personalpeach10@icloud.com>
2022-06-20 17:29:09 +0900

    update: 検索機能をタイトル検索からキーワード・タグ検索に変更 update: トップページのゲストログインをログイン後はログイン済みと表示されるように変更
    内容: ransack機能を用いた検索機能なので、検索するカラムの範囲を増やした。
    タグは別テーブルではあるが、1対多の関係なのでテーブルを指定して検索できるようにした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_search
+  before_action :set_search, only: [:index, :search, :show]
 
   private
   def after_sign_out_path_for(resource)
